371a36fe5062acb78c082441109efb5242d16530
Jody Wan <jody.kpw@gmail.com>
2019-04-03 17:49:29 +0800

    first commit

---
+++ vagrant/provision-cm-redhat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Update & Upgrade CentOS
+yum update -y
+yum upgrade -y
+
+# Enable Software Collections (SCL)
+sudo yum install -y centos-release-scl
+
+# Installing Python 3 on CentOS 7
+sudo yum install -y rh-python36
+
+scl enable rh-python36 bash
+
+# Installing Development Tools
+sudo yum groupinstall 'Development Tools'
+
+# Install python-pip and any required packages
+sudo yum -y install python-pip
+
+# Install GIT
+sudo yum install -y git
+
+# Install Ansible
+sudo yum install -y ansible
+
+# Ansible
+su -c  "ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''" root
+cp /root/.ssh/id_rsa.pub /provision/cm/ssh-key/
